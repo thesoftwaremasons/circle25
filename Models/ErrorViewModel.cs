bb493939e70236f93031fe50ae4694b7d950c3ad
thesoftwaremasons <smiles2k6@gmail.com>
2019-03-26 15:25:54 +0100

    commit all

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace circle25.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
